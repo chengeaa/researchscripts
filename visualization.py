75fb3317054c84a35fe36077c9a645ae21ed30a2
Erik Cheng <ckaiwen2@hotmail.com>
2021-03-29 14:36:02 -0500

    add plotElemDist function to visualization.py, removed plotElemDist and readData from C focus nb

--- visualization.py
+++ visualization.py
@@ -13,3 +13,35 @@ def show_atoms_grid(data, rotation = '-0x,0y,0z', save= False, filename = 'grid_
         plot_atoms(config, axarr[i%dim,i//dim], rotation = rotation)
     if save:
         fig.savefig(filename + ".png")
+
+def plotElemDist(data, targetElem = "C", latticeElems = ["Si", "N", "H"], nbins = 25, stacked = False):
+    """
+    Plot distribution of element within slab, data should be arraylike collection of stuctures    
+    """
+    targetZs = []
+    latticeZs = []
+
+    # populate a cZs list of hists, latticeZs list of hists
+    for key, value in data.iteritems():
+        targetZs += [atom.position[2] for atom in value if atom.symbol == targetElem]
+        latticeZs += [atom.position[2] for atom in value if atom.symbol in latticeElems]
+
+
+    minZ, maxZ = np.min(latticeZs), np.max(latticeZs)
+    bins = np.linspace(minZ, maxZ, nbins)
+    width = (maxZ-minZ)/nbins
+
+    if stacked:
+        h = plt.hist([targetZs, latticeZs], bins = bins, density = True, alpha = 1, 
+                 label = "stacked {} and {} distributions".format(targetElem, latticeElems), stacked = True)
+        plt.vlines([minZ, maxZ], 0, np.max(h[:1]), label = "min and max Z positions")
+    else:
+        h1 = plt.hist(targetZs, bins = bins, density = True, alpha = 0.8, 
+                 label = "{} distribution".format(targetElem))
+        h2 = plt.hist(latticeZs, bins = bins, density = True, alpha = 0.2, 
+                 label = "{} distribution".format(latticeElems))
+        plt.vlines([minZ, maxZ], 0, np.max([h1[:1], h2[:1]]), label = "min and max Z positions")
+
+
+    plt.legend()
+    plt.show()
