3a30dba0bbefc4d9c82cca1f93aa8f6120bc3594
Erik Cheng <ckaiwen2@hotmail.com>
2021-06-14 20:01:29 -0500

    utils import thing

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
 from ase.neighborlist import NewPrimitiveNeighborList, natural_cutoffs
-from utils import readStructs
+from researchscripts.utils import readStructs
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
